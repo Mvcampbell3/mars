35cf9158db693418f2dac9d29b06a841069808d0
Mvcampbell3 <mvcampbell3@gmail.com>
2020-12-18 21:01:53 -0500

    mongoose with atlas connect

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const routes = require('./routes');
+const mongoose = require('mongoose');
 require('dotenv').config()
 
 app.use(express.urlencoded({ extended: true }));
@@ -12,6 +13,14 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.use(routes)
+
+mongoose.connect(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('connected to mongo')
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`server is running on http://localhost:${PORT}`)
